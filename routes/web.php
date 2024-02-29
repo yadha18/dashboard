37ad5c092541ecf4caa1a7838cc701269e42c94a
yadha18 <82631881+yadha18@users.noreply.github.com>
2024-02-29 10:49:41 +0700

    Prod route test

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,16 @@ Route::get('/', function () {
 });
 
 Route::controller(UserController::class)->group(function () {
-    Route::get('/dashboard/register', 'register')->name('register');
-    Route::post('/store', 'store')->name('store');
-    Route::get('/dashboard/login', 'login')->name('login');
-    Route::post('/authenticate', 'authenticate')->name('authenticate');
-    Route::get('/dashboard', 'dashboard')->name('dashboard');
-    Route::get('/passive-customer', 'passivecustomer')->name('passive-customer');
-    Route::get('/dashboard/revenue', 'revenue')->name('revenue');
-    Route::get('/dashboard/kanal-bayar', 'kanalbayar')->name('kanal-bayar');
-    Route::get('/dashboard/pelanggan-deaktivasi', 'pelanggandeaktivasi')->name('pelanggan-deaktivasi');
-    Route::post('/logout', 'logout')->name('logout');
 });
+Route::get('/dashboard/register', 'register')->name('register');
+Route::post('/store', 'store')->name('store');
+Route::get('/dashboard/login', 'login')->name('login');
+Route::post('/authenticate', 'authenticate')->name('authenticate');
+Route::get('/dashboard', 'dashboard')->name('dashboard');
+Route::get('/passive-customer', 'passivecustomer')->name('passive-customer');
+Route::get('/dashboard/revenue', 'App\Http\Controllers\UserController@revenue')->name('revenue');
+Route::get('/dashboard/kanal-bayar', 'kanalbayar')->name('kanal-bayar');
+Route::get('/dashboard/pelanggan-deaktivasi', 'pelanggandeaktivasi')->name('pelanggan-deaktivasi');
+Route::post('/logout', 'logout')->name('logout');
 
 Route::get('/chart-data', 'App\Http\Controllers\ChartController@getChartData');
