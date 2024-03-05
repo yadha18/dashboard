b8d0bcc8fdb0ae47239b6dc7323f8f615cf22e15
yadha18 <82631881+yadha18@users.noreply.github.com>
2024-03-05 09:30:28 +0700

    Kanal Card

--- resources/views/components/kanal-card.blade.php
+++ resources/views/components/kanal-card.blade.php
@@ -1,7 +1,7 @@
 <div class="col-lg-3 col-6">
     <!-- small box -->
     <div class="small-box bg-light">
-        <h5 class="card-header">Mandiri</h5>
+        <h3 class="card-header">Mandiri</h3>
         <div class="inner card-body">
             <p class="card-text">Jumlah tagihan: 502</p>
             <p class="card-text">Jumlah rupiah: Rp. 502.000</p>
