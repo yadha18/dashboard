0e4458932b3d46a9166670271d7154c2aa1ccb50
yadha18 <82631881+yadha18@users.noreply.github.com>
2024-03-05 11:07:34 +0700

    minor fix

--- resources/views/components/kanal-card.blade.php
+++ resources/views/components/kanal-card.blade.php
@@ -3,7 +3,7 @@
     <div class="small-box">
         <h5 class="card-header bg-secondary bg-opacity-50"><strong>{{ $name }}</strong></h5>
         <div class="inner card-body bg-light">
-            <p class="card-text">Jumlah Tagihan: {{ $bill }}</p>
+            <p class="card-text">Jumlah Tagihan: {{ str_replace(',', '.', number_format($bill)) }}</p>
             <p class="card-text">Jumlah Rupiah: Rp. {{ str_replace(',', '.', number_format($money)) }}</p>
         </div>
     </div>
