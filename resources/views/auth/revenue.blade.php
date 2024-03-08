8fb57785f831392a8982db251c968f4dfa044c38
yadha18 <82631881+yadha18@users.noreply.github.com>
2024-03-08 14:37:19 +0700

    fixing UI

--- resources/views/auth/revenue.blade.php
+++ resources/views/auth/revenue.blade.php
@@ -48,13 +48,19 @@
                                 <div class="card-body">
                                     <div class="btn-group" role="group" aria-label="Filter Buttons">
                                         <button type="button" class="btn btn-primary filter-button mx-3" data-year="2023"
-                                            data-type="postpaid">Postpaid 2023</button>
-                                        <button type="button" class="btn btn-primary filter-button mx-3" data-year="2023"
                                             data-type="prepaid">Prepaid 2023</button>
-                                        <button type="button" class="btn btn-primary filter-button mx-3" data-year="2024"
-                                            data-type="postpaid">Postpaid 2024</button>
+                                    </div>
+                                    <div class="btn-group" role="group" aria-label="Filter Buttons">
+                                        <button type="button" class="btn btn-primary filter-button mx-3" data-year="2023"
+                                            data-type="postpaid">Postpaid 2023</button>
+                                    </div>
+                                    <div class="btn-group" role="group" aria-label="Filter Buttons">
                                         <button type="button" class="btn btn-primary filter-button mx-3" data-year="2024"
                                             data-type="prepaid">Prepaid 2024</button>
+                                    </div>
+                                    <div class="btn-group" role="group" aria-label="Filter Buttons">
+                                        <button type="button" class="btn btn-primary filter-button mx-3" data-year="2024"
+                                            data-type="postpaid">Postpaid 2024</button>
                                     </div>
                                     <table id="table-revenue" class="table table-bordered table-striped">
                                         <thead>
