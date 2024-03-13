dda94ac2dde2e84288a1e603bdf603225c34f338
yadha18 <82631881+yadha18@users.noreply.github.com>
2024-03-13 12:57:14 +0700

    Bar Chart test

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -162,54 +162,126 @@
                             sbuLabel.push(item.namaSBU),
                                 totalData.push(item.jumlah)
                         })
-                        var salesChartCanvas = $('#sales-chart').get(0).getContext('2d')
-
-                        var salesChartData = {
-                            labels: sbuLabel,
-                            datasets: [{
-                                label: '2021',
-                                backgroundColor: 'rgba(60,141,188,0.9)',
-                                borderColor: 'rgba(60,141,188,0.8)',
-                                pointRadius: false,
-                                pointColor: '#3b8bba',
-                                pointStrokeColor: 'rgba(60,141,188,1)',
-                                pointHighlightFill: '#fff',
-                                pointHighlightStroke: 'rgba(60,141,188,1)',
-                                data: totalData
-                            }]
+                        var ticksStyle = {
+                            fontColor: '#495057',
+                            fontStyle: 'bold'
                         }
 
-                        var salesChartOptions = {
-                            maintainAspectRatio: false,
-                            responsive: true,
-                            legend: {
-                                display: false
-                            },
-                            scales: {
-                                xAxes: [{
-                                    gridLines: {
-                                        display: false
-                                    }
-                                }],
-                                yAxes: [{
-                                    gridLines: {
-                                        display: false
-                                    }
-                                }]
-                            }
-                        }
+                        var mode = 'index'
+                        var intersect = true
 
-                        // This will get the first returned node in the jQuery collection.
+                        var $salesChart = $('#sales-chart')
                         // eslint-disable-next-line no-unused-vars
-                        var salesChart = new Chart(salesChartCanvas, {
-                            type: 'line',
-                            data: salesChartData,
-                            options: salesChartOptions
+                        var salesChart = new Chart($salesChart, {
+                            type: 'bar',
+                            data: {
+                                labels: sbuLabel,
+                                datasets: [{
+                                        backgroundColor: '#007bff',
+                                        borderColor: '#007bff',
+                                        data: totalData
+                                    },
+                                    {
+                                        backgroundColor: '#ced4da',
+                                        borderColor: '#ced4da',
+                                        data: [700, 1700, 2700, 2000, 1800, 1500, 2000]
+                                    }
+                                ]
+                            },
+                            options: {
+                                maintainAspectRatio: false,
+                                tooltips: {
+                                    mode: mode,
+                                    intersect: intersect
+                                },
+                                hover: {
+                                    mode: mode,
+                                    intersect: intersect
+                                },
+                                legend: {
+                                    display: false
+                                },
+                                scales: {
+                                    yAxes: [{
+                                        // display: false,
+                                        gridLines: {
+                                            display: true,
+                                            lineWidth: '4px',
+                                            color: 'rgba(0, 0, 0, .2)',
+                                            zeroLineColor: 'transparent'
+                                        },
+                                        ticks: $.extend({
+                                            beginAtZero: true,
+
+                                            // Include a dollar sign in the ticks
+                                            callback: function(value) {
+                                                if (value >= 1000) {
+                                                    value /= 1000
+                                                    value += 'k'
+                                                }
+
+                                                return '$' + value
+                                            }
+                                        }, ticksStyle)
+                                    }],
+                                    xAxes: [{
+                                        display: true,
+                                        gridLines: {
+                                            display: false
+                                        },
+                                        ticks: ticksStyle
+                                    }]
+                                }
+                            }
                         })
                     }
                 })
+
             }
-            fetchData();
+            var salesChartCanvas = $('#salesChart').get(0).getContext('2d')
+
+            var salesChartData = {
+                labels: sbuLabel,
+                datasets: [{
+                    label: '2021',
+                    backgroundColor: 'rgba(60,141,188,0.9)',
+                    borderColor: 'rgba(60,141,188,0.8)',
+                    pointRadius: false,
+                    pointColor: '#3b8bba',
+                    pointStrokeColor: 'rgba(60,141,188,1)',
+                    pointHighlightFill: '#fff',
+                    pointHighlightStroke: 'rgba(60,141,188,1)',
+                    data: totalData
+                }]
+            }
+
+            var salesChartOptions = {
+                maintainAspectRatio: false,
+                responsive: true,
+                legend: {
+                    display: false
+                },
+                scales: {
+                    xAxes: [{
+                        gridLines: {
+                            display: false
+                        }
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            display: false
+                        }
+                    }]
+                }
+            }
+
+            // This will get the first returned node in the jQuery collection.
+            // eslint-disable-next-line no-unused-vars
+            var salesChart = new Chart(salesChartCanvas, {
+                type: 'line',
+                data: salesChartData,
+                options: salesChartOptions
+            })
         })
     </script>
     <script>
