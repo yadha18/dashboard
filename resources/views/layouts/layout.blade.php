50649f15d599f0f3b02e95ee3eae039a475867b0
yadha18 <82631881+yadha18@users.noreply.github.com>
2024-03-13 11:58:46 +0700

    bar chart Test

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -151,74 +151,65 @@
     <script>
         $(function() {
             function fetchData() {
-                url: '/get-baddebt-2021',
-                method: 'GET',
-                success: function(data) {
-                    var sbuLabel = [];
-                    var totalData = [];
+                $.ajax({
+                    url: '/get-baddebt-2021',
+                    method: 'GET',
+                    success: function(data) {
+                        var sbuLabel = [];
+                        var totalData = [];
 
-                    data.forEach((item) => {
-                        sbuLabel.push(item.namaSBU),
-                            totalData.push(item.jumlah)
-                    })
-                }
-            }
-            var salesChartCanvas = $('#salesChart').get(0).getContext('2d')
+                        data.forEach((item) => {
+                            sbuLabel.push(item.namaSBU),
+                                totalData.push(item.jumlah)
+                        })
+                        var salesChartCanvas = $('#salesChart').get(0).getContext('2d')
 
-            var salesChartData = {
-                labels: sbuLabel,
-                datasets: [{
-                        label: '2021',
-                        backgroundColor: 'rgba(60,141,188,0.9)',
-                        borderColor: 'rgba(60,141,188,0.8)',
-                        pointRadius: false,
-                        pointColor: '#3b8bba',
-                        pointStrokeColor: 'rgba(60,141,188,1)',
-                        pointHighlightFill: '#fff',
-                        pointHighlightStroke: 'rgba(60,141,188,1)',
-                        data: totalData
-                    },
-                    {
-                        label: 'Electronics',
-                        backgroundColor: 'rgba(210, 214, 222, 1)',
-                        borderColor: 'rgba(210, 214, 222, 1)',
-                        pointRadius: false,
-                        pointColor: 'rgba(210, 214, 222, 1)',
-                        pointStrokeColor: '#c1c7d1',
-                        pointHighlightFill: '#fff',
-                        pointHighlightStroke: 'rgba(220,220,220,1)',
-                        data: [65, 59, 80, 81, 56, 55, 40]
+                        var salesChartData = {
+                            labels: sbuLabel,
+                            datasets: [{
+                                label: '2021',
+                                backgroundColor: 'rgba(60,141,188,0.9)',
+                                borderColor: 'rgba(60,141,188,0.8)',
+                                pointRadius: false,
+                                pointColor: '#3b8bba',
+                                pointStrokeColor: 'rgba(60,141,188,1)',
+                                pointHighlightFill: '#fff',
+                                pointHighlightStroke: 'rgba(60,141,188,1)',
+                                data: totalData
+                            }]
+                        }
+
+                        var salesChartOptions = {
+                            maintainAspectRatio: false,
+                            responsive: true,
+                            legend: {
+                                display: false
+                            },
+                            scales: {
+                                xAxes: [{
+                                    gridLines: {
+                                        display: false
+                                    }
+                                }],
+                                yAxes: [{
+                                    gridLines: {
+                                        display: false
+                                    }
+                                }]
+                            }
+                        }
+
+                        // This will get the first returned node in the jQuery collection.
+                        // eslint-disable-next-line no-unused-vars
+                        var salesChart = new Chart(salesChartCanvas, {
+                            type: 'line',
+                            data: salesChartData,
+                            options: salesChartOptions
+                        })
                     }
-                ]
+                })
             }
-
-            var salesChartOptions = {
-                maintainAspectRatio: false,
-                responsive: true,
-                legend: {
-                    display: false
-                },
-                scales: {
-                    xAxes: [{
-                        gridLines: {
-                            display: false
-                        }
-                    }],
-                    yAxes: [{
-                        gridLines: {
-                            display: false
-                        }
-                    }]
-                }
-            }
-
-            // This will get the first returned node in the jQuery collection.
-            // eslint-disable-next-line no-unused-vars
-            var salesChart = new Chart(salesChartCanvas, {
-                type: 'line',
-                data: salesChartData,
-                options: salesChartOptions
-            })
+            fetchData();
         })
     </script>
     <script>
