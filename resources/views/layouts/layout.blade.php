5dc4a058583a91f0631d6e0f2421846ba1fb8609
yadha18 <82631881+yadha18@users.noreply.github.com>
2024-03-13 13:05:06 +0700

    Bar Chart Test

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -150,109 +150,113 @@
     </script>
     <script>
         $(function() {
-            function fetchData() {
-                $.ajax({
-                    url: '/get-baddebt-2021',
-                    method: 'GET',
-                    success: function(data) {
-                        var sbuLabel = [];
-                        var totalData = [];
+            'use strict';
 
-                        data.forEach((item) => {
-                            sbuLabel.push(item.namaSBU),
-                                totalData.push(item.jumlah)
-                        })
-                        var ticksStyle = {
-                            fontColor: '#495057',
-                            fontStyle: 'bold'
-                        }
-
-                        var mode = 'index'
-                        var intersect = true
-
-                        var $salesChart = $('#sales-chart')
-                        // eslint-disable-next-line no-unused-vars
-                        var salesChart = new Chart($salesChart, {
-                            type: 'bar',
-                            data: {
-                                labels: sbuLabel,
-                                datasets: [{
-                                        backgroundColor: '#007bff',
-                                        borderColor: '#007bff',
-                                        data: totalData
-                                    },
-                                    {
-                                        backgroundColor: '#ced4da',
-                                        borderColor: '#ced4da',
-                                        data: [700, 1700, 2700, 2000, 1800, 1500, 2000]
-                                    }
-                                ]
-                            },
-                            options: {
-                                maintainAspectRatio: false,
-                                tooltips: {
-                                    mode: mode,
-                                    intersect: intersect
-                                },
-                                hover: {
-                                    mode: mode,
-                                    intersect: intersect
-                                },
-                                legend: {
-                                    display: false
-                                },
-                                scales: {
-                                    yAxes: [{
-                                        // display: false,
-                                        gridLines: {
-                                            display: true,
-                                            lineWidth: '4px',
-                                            color: 'rgba(0, 0, 0, .2)',
-                                            zeroLineColor: 'transparent'
-                                        },
-                                        ticks: $.extend({
-                                            beginAtZero: true,
-
-                                            // Include a dollar sign in the ticks
-                                            callback: function(value) {
-                                                if (value >= 1000) {
-                                                    value /= 1000
-                                                    value += 'k'
-                                                }
-
-                                                return '$' + value
-                                            }
-                                        }, ticksStyle)
-                                    }],
-                                    xAxes: [{
-                                        display: true,
-                                        gridLines: {
-                                            display: false
-                                        },
-                                        ticks: ticksStyle
-                                    }]
-                                }
-                            }
-                        })
-                    }
-                })
-
+            var ticksStyle = {
+                fontColor: '#495057',
+                fontStyle: 'bold'
             }
+
+            var mode = 'index';
+            var intersect = true;
+
+            var $salesChart = $('#sales-chart');
+
+            $.ajax({
+                url: '/get-baddebt-2021',
+                method: 'GET',
+                success: function(response) {
+                    var namaSBU = response.map(function(data) {
+                        return data.namaSBU;
+                    });
+
+                    var jumlah = response.map(function(data) {
+                        return data.jumlah;
+                    });
+
+                    var salesChart = new Chart($salesChart, {
+                        type: 'bar',
+                        data: {
+                            labels: namaSBU,
+                            datasets: [{
+                                backgroundColor: '#007bff',
+                                borderColor: '#007bff',
+                                data: jumlah
+                            }]
+                        },
+                        options: {
+                            maintainAspectRatio: false,
+                            tooltips: {
+                                mode: mode,
+                                intersect: intersect
+                            },
+                            hover: {
+                                mode: mode,
+                                intersect: intersect
+                            },
+                            legend: {
+                                display: false
+                            },
+                            scales: {
+                                yAxes: [{
+                                    gridLines: {
+                                        display: true,
+                                        lineWidth: '4px',
+                                        color: 'rgba(0, 0, 0, .2)',
+                                        zeroLineColor: 'transparent'
+                                    },
+                                    ticks: $.extend({
+                                        beginAtZero: true,
+                                        callback: function(value) {
+                                            if (value >= 1000) {
+                                                value /= 1000;
+                                                value += 'k';
+                                            }
+                                            return '$' + value;
+                                        }
+                                    }, ticksStyle)
+                                }],
+                                xAxes: [{
+                                    display: true,
+                                    gridLines: {
+                                        display: false
+                                    },
+                                    ticks: ticksStyle
+                                }]
+                            }
+                        }
+                    });
+                }
+            })
+
+
             var salesChartCanvas = $('#salesChart').get(0).getContext('2d')
 
             var salesChartData = {
-                labels: sbuLabel,
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
                 datasets: [{
-                    label: '2021',
-                    backgroundColor: 'rgba(60,141,188,0.9)',
-                    borderColor: 'rgba(60,141,188,0.8)',
-                    pointRadius: false,
-                    pointColor: '#3b8bba',
-                    pointStrokeColor: 'rgba(60,141,188,1)',
-                    pointHighlightFill: '#fff',
-                    pointHighlightStroke: 'rgba(60,141,188,1)',
-                    data: totalData
-                }]
+                        label: '2021',
+                        backgroundColor: 'rgba(60,141,188,0.9)',
+                        borderColor: 'rgba(60,141,188,0.8)',
+                        pointRadius: false,
+                        pointColor: '#3b8bba',
+                        pointStrokeColor: 'rgba(60,141,188,1)',
+                        pointHighlightFill: '#fff',
+                        pointHighlightStroke: 'rgba(60,141,188,1)',
+                        data: [28, 48, 40, 19, 86, 27, 90]
+                    },
+                    {
+                        label: 'Electronics',
+                        backgroundColor: 'rgba(210, 214, 222, 1)',
+                        borderColor: 'rgba(210, 214, 222, 1)',
+                        pointRadius: false,
+                        pointColor: 'rgba(210, 214, 222, 1)',
+                        pointStrokeColor: '#c1c7d1',
+                        pointHighlightFill: '#fff',
+                        pointHighlightStroke: 'rgba(220,220,220,1)',
+                        data: [65, 59, 80, 81, 56, 55, 40]
+                    }
+                ]
             }
 
             var salesChartOptions = {
@@ -282,7 +286,6 @@
                 data: salesChartData,
                 options: salesChartOptions
             })
-            fetchData();
         })
     </script>
     <script>
