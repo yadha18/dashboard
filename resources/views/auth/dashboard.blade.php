9593a8830376547678887d164d11a1c325561c7e
yadha18 <82631881+yadha18@users.noreply.github.com>
2024-03-14 14:57:22 +0700

    Chart UI Test

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -116,7 +116,7 @@
                                 <!-- ./card-body -->
                                 <div class="card-footer">
                                     <div class="row">
-                                        <div class="col-sm-3 col-6">
+                                        <div class="col-sm-4 col-7">
                                             <div class="description-block border-right">
                                                 <span class="description-percentage text-success"><i
                                                         class="fas fa-caret-up"></i> 0%</span>
@@ -127,7 +127,7 @@
                                             <!-- /.description-block -->
                                         </div>
                                         <!-- /.col -->
-                                        <div class="col-sm-3 col-6">
+                                        <div class="col-sm-4 col-7">
                                             <div class="description-block border-right">
                                                 <span class="description-percentage text-warning"><i
                                                         class="fas fa-caret-left"></i> 0%</span>
@@ -137,22 +137,12 @@
                                             <!-- /.description-block -->
                                         </div>
                                         <!-- /.col -->
-                                        <div class="col-sm-3 col-6">
+                                        <div class="col-sm-4 col-7">
                                             <div class="description-block border-right">
                                                 <span class="description-percentage text-success"><i
                                                         class="fas fa-caret-up"></i> 20%</span>
                                                 <h5 class="description-header">$24,813.53</h5>
                                                 <span class="description-text">TOTAL PROFIT</span>
-                                            </div>
-                                            <!-- /.description-block -->
-                                        </div>
-                                        <!-- /.col -->
-                                        <div class="col-sm-3 col-6">
-                                            <div class="description-block">
-                                                <span class="description-percentage text-danger"><i
-                                                        class="fas fa-caret-down"></i> 18%</span>
-                                                <h5 class="description-header">1200</h5>
-                                                <span class="description-text">GOAL COMPLETIONS</span>
                                             </div>
                                             <!-- /.description-block -->
                                         </div>
