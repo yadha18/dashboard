fc73fdd3fefd343e66121d1e9fbaf8141887973d
yadha18 <82631881+yadha18@users.noreply.github.com>
2024-03-26 13:41:06 +0700

    endpoint URL test

--- app/Http/Controllers/RevenueController.php
+++ app/Http/Controllers/RevenueController.php
@@ -145,7 +145,20 @@ class RevenueController extends Controller
         $data = Revenue::selectRaw('SUM(pendapatan) AS pendapatan, bulan, tahun, namaLayananProduk')
             ->whereIn('namaLayananProduk', ['5 MBPS', '10 MBPS', '20 MBPS', '35 MBPS', '50 MBPS', '100 MBPS'])
             ->groupBy('bulan', 'tahun', 'namaLayananProduk')
-            ->orderByRaw('FIELD(bulan, "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember")')
+            ->orderByRaw('CASE bulan
+                        WHEN "January" THEN 1
+                        WHEN "February" THEN 2
+                        WHEN "March" THEN 3
+                        WHEN "April" THEN 4
+                        WHEN "May" THEN 5
+                        WHEN "June" THEN 6
+                        WHEN "Jule" THEN 7
+                        WHEN "August" THEN 8
+                        WHEN "September" THEN 9
+                        WHEN "October" THEN 10
+                        WHEN "November" THEN 11
+                        WHEN "December" THEN 12
+                        ELSE 999 END')
             ->get();
 
         return response()->json($data);
