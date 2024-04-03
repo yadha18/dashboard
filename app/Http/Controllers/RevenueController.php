c6e9061cd22ee2f775df2944a2c6624cc5ed3eda
yadha18 <82631881+yadha18@users.noreply.github.com>
2024-04-03 11:29:05 +0700

    getDailyRevenue test

--- app/Http/Controllers/RevenueController.php
+++ app/Http/Controllers/RevenueController.php
@@ -73,8 +73,8 @@ class RevenueController extends Controller
     public function getDailyRevenue()
     {
         $currentDate = Carbon::now();
-        $startDate = $currentDate->subDays(7)->format('Y-m-d');
-        $endDate = $currentDate->subDays(3)->format('Y-m-d');
+        $startDate = $currentDate->subDays(10)->format('Y-m-d');
+        $endDate = $currentDate->subDays(4)->format('Y-m-d');
 
         $dailyRevenueQuery = Revenue::select(DB::raw("FORMAT(tanggalBayar, 'yyyy-MM-dd') AS tanggalBayar"))->selectRaw("SUM(pendapatan) AS pendapatanHarian")->whereBetween('tanggalBayar', [$startDate, $endDate])->groupBy(DB::raw("FORMAT(tanggalBayar, 'yyyy-MM-dd')"))->get();
 
@@ -106,8 +106,8 @@ class RevenueController extends Controller
     public function dailyRevenue()
     {
         $currentDate = Carbon::now();
-        $startDate = $currentDate->subDays(7)->format('Y-m-d');
-        $endDate = $currentDate->subDays(3)->format('Y-m-d');
+        $startDate = $currentDate->subDays(10)->format('Y-m-d');
+        $endDate = $currentDate->subDays(4)->format('Y-m-d');
 
         $user = User::select('name')->first();
         $daily = Revenue::select('idTagihan', 'pendapatan', 'typeBilling', 'tanggalBayar', 'bulan', 'tahun', 'namaLayanan', 'namaLayananProduk')->where('bulan', 'February')->where('tahun', 2024)->take(500)->get();
